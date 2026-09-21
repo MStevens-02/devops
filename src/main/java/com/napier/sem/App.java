919e99bd36c043ec06ca2480dbde8dd127b13bd5
Michael Stevens <michaelalexstevens@gmail.com>
2026-09-21 19:25:31 +0100

    MySQL Server Setup and Connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,9 +1,16 @@
 package com.napier.sem;
 
-public class App
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Boo yah!!");
+public class App {
+    public static void main(String[] args) {
+        // Create new Database Connector
+        DatabaseConnector con = new DatabaseConnector();
+        // Connect to database
+        con.connect();
+
+        // ... your application logic goes here ...
+
+        // Disconnect from database
+        System.out.println("Test");
+        con.disconnect();
     }
 }
